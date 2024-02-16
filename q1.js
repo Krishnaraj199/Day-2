827743bee4a302126b0e32594f1af821faf3aed3
krishnaraj199 <kishorekrish255@gmail.com>
2024-02-16 14:12:44 +0530

    added a new file

---
+++ q1.js
@@ -0,0 +1,6 @@
+/* The document object represents the web page that is loaded in the browser.
+It provides access to the elements within the HTML page.
+Through the document object, we can dynamically manipulate and add content to our web page.
+The window object represents the entire browser window or tab.
+It encompasses properties, methods, and functionalities related to the browser window.
+The global object in JavaScript is the window object. */
